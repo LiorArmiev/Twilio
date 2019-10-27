790ae294589872f4b776c6af23d1a2cc8c552e3f
Lior Armiev <lior.a@cloudvalley.io>
2019-10-27 14:39:51 +0200

    update

--- call.py
+++ call.py
@@ -11,16 +11,18 @@ twiliohtml+= alerturl
 print(phonenumber)
 print(twiliohtml)
 
-#account_sid = ''
-#auth_token = ''
-client = Client(account_sid, auth_token)
+if(phonenumber is not None and alertcontext is not None):
+    #account_sid = ''
+    #auth_token = ''
+    client = Client(account_sid, auth_token)
 
-call = client.calls.create(
-                        url=twiliohtml,
-                        to=phonenumber,
-                        from_='yyyy'
-                    )
-#output
-print(call.sid)
-
+    call = client.calls.create(
+                            url=twiliohtml,
+                            to=phonenumber,
+                            from_='yyyy'
+                        )
+    #output
+    print(call.sid)
+else:
+    print("No Input Data")
 #test feature
